7d9a8ea4179ccada899aa16edd3ea47da196e07a
Ricardo Taverna <taverna.ricardo@gmail.com>
2022-03-30 00:58:48 -0300

    [MOV-002] - adicionado teste unitario para testar a adição de um usuario cliente.

--- backend/src/usuarios/tests.py
+++ backend/src/usuarios/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from os import name
+from django.urls import reverse
+from django.contrib.auth.models import User, Group
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class AccountTests(APITestCase):
+    def setUp(self):
+        Group.objects.create(name="cliente")
+
+    def test_create_account(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = '/usuarios/cliente/'
+        data = {
+            'username':'gnomoteste',
+            'first_name':'Gnomo',
+            'last_name':'Teste',
+            'email':'gnomedeteste@teste.com',
+            'password':'senhadognomo1234',
+            'groupname': 'cliente'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'gnomoteste')
