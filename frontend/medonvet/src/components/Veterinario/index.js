498f6f2dda46377a38897d61afd7f52895ac3ba7
Stengrat <ygorsa1998@hotmail.com>
2022-06-04 18:26:09 -0300

    [MOV-016] Alterações no form

--- frontend/medonvet/src/components/Veterinario/index.js
+++ frontend/medonvet/src/components/Veterinario/index.js
@@ -17,7 +17,6 @@ import { api } from '../../services/api';
 
 export class Veterinario extends React.Component {
 
-    _isMounted = false;
 
     emptyVet = {
         id: null,
@@ -43,7 +42,8 @@ export class Veterinario extends React.Component {
             submitted: false,
             globalFilter: null,
             loading: true,
-            totalSize: 0
+            totalSize: 0,
+            idAux: 0,
         };
         this.onLoad = this.onLoad.bind(this);
         this.openNew = this.openNew.bind(this);
@@ -65,8 +65,10 @@ export class Veterinario extends React.Component {
             this.onLoad();
     }
 
-    componentDidUpdate() {
+    componentDidUpdate(prevProps, prevState) {
+        if(this.state.idAux !== prevState.idAux){
             this.onLoad();
+        }
     }
 
 
@@ -85,58 +87,56 @@ export class Veterinario extends React.Component {
     }
 
     saveVet = async e => {
-        if(this._isMounted){
             let state = { submitted: true };
             let vets = [...this.state.vets];
             let vet = {...this.state.vet};
-            
-            if ( !vet.username ) {
+        
+            if ( !vet.first_name ) {
                 this.setState(
-                    {messageError: "O Nome do pet é obrigatório para realizar o cadastro. W.W"}
+                    {messageError: "O Nome do Vet é obrigatório para realizar o cadastro. W.W"}
                 );
-                this.toast.show({ severity: 'error', summary: 'Erro', detail: 'O campo nome é obrigatório para o cadastro do pet.', life: 3000 });
+                this.toast.show({ severity: 'error', summary: 'Erro', detail: 'O campo nome é obrigatório para o cadastro do vet.', life: 3000 });
             } else {
 
-                if(this.state.pet.id){
-                    const index = this.findIndexById(this.state.pet.id);
+                if(this.state.vet.id){
+                    const index = this.findIndexById(this.state.vet.id);
                     vets[index] = vet;
                     try {
-                        api.put(`/prestadores/veterinario/${this.state.pet.id}/`, vet).then(response => console.log(response))
-                        this.toast.show({ severity: 'success', summary: 'Successful', detail: 'Pet atualizado', life: 3000 });
+                        api.put(`/prestadores/veterinario/${this.state.vet.id}/`, vet).then(response => console.log(response))
+                        this.toast.show({ severity: 'success', summary: 'Successful', detail: 'Veterinario atualizado', life: 3000 });
                     } catch (err) {
                         console.log(`Erro: ${err}`)
                     }
 
                 } else {
                     try {
-                        api.post('/prestadores/veterinario/', vet).then(response => console.log(response, vet))
-                        this.toast.show({ severity: 'success', summary: 'Successful', detail: 'Pet adicionado', life: 3000 });
+                        api.post('/prestadores/veterinario/', vet).then(response => {
+                            console.log(response, vet)
+                            this.setState(prevState => ({id: prevState.idAux +1}))
+                        })
+                        this.toast.show({ severity: 'success', summary: 'Successful', detail: 'Veterinario adicionado', life: 3000 });
                     } catch (err) {
                         console.log(`Erro: ${err}`)
-                        console.log(`Pet: ${vet}`)
+                        console.log(`Veterinario: ${vet}`)
                     }
                 }
 
                 state = {
                     ...state,
                     vets,
-                    petDialog: false,
-                    pet: this.emptyPet
+                    vetDialog: false,
+                    vet: this.emptyVet
                 };
         
                 this.setState(state);
             }
-        }
-
     }
 
     editVet(vet) {
-        if(this._isMounted){
             this.setState({
                 vet: { ...vet },
                 vetDialog: true
             });
-        }
     }
 
     confirmDeleteVet(vet) {
@@ -147,22 +147,20 @@ export class Veterinario extends React.Component {
     }
 
     deleteVet() {
-        if(this._isMounted){
-            let vets = this.state.vets.filter(val => val.id !== this.state.vet.id);
-    
-            api.delete(`/prestadores/veterinario/${this.state.vet.id}/`)
-            this.setState({
-                vets,
-                deletePetDialog: false,
-                vet: this.emptyPet
-            });
-            this.toast.show({ severity: 'success', summary: 'Successful', detail: 'Pet deletado', life: 3000 });
-        }
+        let vets = this.state.vets.filter(val => val.id !== this.state.vet.id);
+
+        api.delete(`/prestadores/veterinario/${this.state.vet.id}/`)
+        this.setState({
+            vets,
+            deleteVetDialog: false,
+            vet: this.emptyVet
+        });
+        this.toast.show({ severity: 'success', summary: 'Successful', detail: 'Veterinario deletado', life: 3000 });
     }
 
 
     findIndexById(id) {
-        let index = -1;
+        let index = - 1;
         for (let i = 0; i < this.state.vets.length; i++) {
             if (this.state.vets[i].id === id) {
                 index = i;
@@ -177,7 +175,7 @@ export class Veterinario extends React.Component {
         this.setState({
             vet: this.emptyVet,
             submitted: false,
-            petDialog: true
+            vetDialog: true
         });
     }
 
@@ -219,8 +217,8 @@ export class Veterinario extends React.Component {
     actionBodyTemplate(rowData) {
         return (
             <React.Fragment>
-                <Button icon="pi pi-pencil" className="p-button-rounded button-primary mr-2" onClick={() => this.editPet(rowData)} />
-                <Button icon="pi pi-trash" className="p-button-rounded p-button-secondary" onClick={() => this.confirmDeletePet(rowData)} />
+                <Button icon="pi pi-pencil" className="p-button-rounded button-primary mr-2" onClick={() => this.editVet(rowData)} />
+                <Button icon="pi pi-trash" className="p-button-rounded p-button-secondary" onClick={() => this.confirmDeleteVet(rowData)} />
             </React.Fragment>
         );
     }
@@ -248,12 +246,12 @@ export class Veterinario extends React.Component {
         const table = (
             <React.Fragment>
                 <Toolbar className="mb-4" left={this.leftToolbarTemplate}></Toolbar>
-                <DataTable ref={(el) => this.dt = el} value={this.state.vets} selection={this.state.selectedVets} onSelectionChange={(e) => this.setState({ selectedPets: e.value })}
+                <DataTable ref={(el) => this.dt = el} value={this.state.vets} selection={this.state.selectedVets} onSelectionChange={(e) => this.setState({ selectedVets: e.value })}
                     dataKey="id" paginator rows={10} rowsPerPageOptions={[5, 10, 25]}
                     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                     currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
                     // globalFilter={this.state.globalFilter} header={header} responsiveLayout="scroll"
-                    >
+                     w >
                     <Column field="imagem" header="Imagem" body={this.imageBodyTemplate} ></Column>
                     <Column field="id" header="Código" sortable style={{ minWidth: '12rem' }}></Column>
                     <Column field="first_name" header="Nome" sortable style={{ minWidth: '16rem' }}></Column>
@@ -277,17 +275,15 @@ export class Veterinario extends React.Component {
 
         const skeleton = (
             <React.Fragment>
-
                 <Skeleton className="mb-4" width="7rem" height="3rem"></Skeleton>
-
                 <DataTable value={Array.from({ length: 5 })} className="p-datatable-striped">
                     <Column field="imagem" header="Imagem" body={<Skeleton shape="circle" size="3rem" className="mr-2"></Skeleton>} ></Column>
                     <Column field="id" header="Código" sortable style={{ minWidth: '12rem' }} body={<Skeleton></Skeleton>}></Column>
-                    <Column field="nome" header="Nome" sortable style={{ minWidth: '16rem' }} body={<Skeleton></Skeleton>}></Column>
-                    <Column field="peso" header="Peso" sortable style={{ minWidth: '8rem' }} body={<Skeleton></Skeleton>}></Column>
-                    <Column field="raca" header="Raça" sortable style={{ minWidth: '10rem' }} body={<Skeleton></Skeleton>}></Column>
-                    <Column field="idade" header="Idade" sortable style={{ minWidth: '12rem' }} body={<Skeleton></Skeleton>}></Column>
-                    <Column field="sexo" header="Sexo" sortable style={{ minWidth: '12rem' }} body={<Skeleton></Skeleton>}></Column>
+                    <Column field="first_name" header="Nome" sortable style={{ minWidth: '16rem' }} body={<Skeleton></Skeleton>}></Column>
+                    <Column field="last_name" header="Sobrenome" sortable style={{ minWidth: '8rem' }} body={<Skeleton></Skeleton>}></Column>
+                    <Column field="username" header="Usuario" sortable style={{ minWidth: '10rem' }} body={<Skeleton></Skeleton>}></Column>
+                    <Column field="crmv" header="CRMV" sortable style={{ minWidth: '12rem' }} body={<Skeleton></Skeleton>}></Column>
+                    <Column field="cpf_cnpj" header="CPF-CNPJ" sortable style={{ minWidth: '12rem' }} body={<Skeleton></Skeleton>}></Column>
                     <Column exportable={false} style={{ minWidth: '12rem' }} body={skeletonActionButtons}></Column>
                 </DataTable>
 
@@ -303,16 +299,13 @@ export class Veterinario extends React.Component {
 
         return(
             
-
             <React.Fragment>
                 <div className="p-6">
                     <div className="datatable-crud-demo">
                         <Toast ref={(el) => this.toast = el} />
 
                         <div className="card">
-
                             {datatable}
-                            
                         </div>
                     </div>
                 </div>
@@ -321,33 +314,60 @@ export class Veterinario extends React.Component {
                                     key={veterinario.id}
                                     id={veterinario.id}
                                     first_name={veterinario.user.first_name}
-                                    last_name={veterinario.user.last_name}>
+                                    last_name={veterinario.user.last_name}
+                                    username={veterinario.user.username}>
                                 </VeterinariosCard>
                             )}
-                <Dialog visible={this.state.vetialog} style={{ width: '750px' }} header="Product Details" modal className="p-fluid" footer={vetDialogFooter} onHide={this.hideDialog}>
+                <Dialog visible={this.state.vetDialog} style={{ width: '750px' }} header="Veterinario Details" modal className="p-fluid" footer={vetDialogFooter} onHide={this.hideDialog}>
                     {/* {this.state.pet.imagem && <img src={`http://127.0.0.1:8000${this.state.pet.imagem}`} onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} alt={this.state.pet.imagem} className="product-image block m-auto pb-3" />} */}
                     
                     <div className="field">
-                        <label htmlFor="nome">Nome</label>
-                        <InputText id="nome" value={this.state.vets.first_name} onChange={(e) => this.onInputChange(e, 'nome')} required autoFocus className={classNames({ 'p-invalid': this.state.submitted && !this.state.vets.first_name })} />
-                        {this.state.submitted && !this.state.vets.first_name && <small className="p-error">Nome é obrigatório.</small>}
+                        <label htmlFor="first_name">Nome</label>
+                        <InputText id="first_name" value={this.state.vet.first_name} onChange={(e) => this.onInputChange(e, 'first_name')} required autoFocus className={classNames({ 'p-invalid': this.state.submitted && !this.state.vet.first_name })} />
+                        {this.state.submitted && !this.state.vet.first_name && <small className="p-error">Nome é obrigatório.</small>}
                     </div>
 
                     <div className="field">
-                        <label htmlFor="raca">Raça</label>
-                        <InputText id="raca" value={this.state.vet.last_name} onChange={(e) => this.onInputChange(e, 'raca')} required autoFocus className={classNames({ 'p-invalid': this.state.submitted && !this.state.vet.last_name })} />
-                        {this.state.submitted && !this.state.vet.last_name && <small className="p-error">Raça é obrigatória.</small>}
+                        <label htmlFor="last_name">Sobrenome</label>
+                        <InputText id="last_name" value={this.state.vet.last_name} onChange={(e) => this.onInputChange(e, 'sobrenome')} required autoFocus className={classNames({ 'p-invalid': this.state.submitted && !this.state.vet.last_name })} />
+                        {this.state.submitted && !this.state.vet.last_name && <small className="p-error">Sobrenome é obrigatória.</small>}
                     </div>
 
                     <div className="formgrid grid">
                         <div className="field col">
-                            <label htmlFor="peso">Peso</label>
-                            <InputNumber id="peso" value={this.state.vet.email} onValueChange={(e) => this.onInputNumberChange(e, 'peso')} />
+                            <label htmlFor="email">E-mail</label>
+                            <InputText id="email" value={this.state.vet.email} onChange={(e) => this.onInputChange(e, 'email')} required autoFocus className={classNames({ 'p-invalid': this.state.submitted && !this.state.vet.email })} />
+                            {this.state.submitted && !this.state.vet.email && <small className="p-error">E-mail é obrigatória.</small>}
                         </div>
                         <div className="field col">
-                            <label htmlFor="sexo">Sexo</label>
-                            <InputText id="sexo" value={this.state.vet.username} onValueonChange={(e) => this.onInputChange(e, 'sexo')} required autoFocus className={classNames({ 'p-invalid': this.state.submitted && !this.state.vet.username })} />
-                            {this.state.submitted && !this.state.vet.username && <small className="p-error">Sexo é obrigatório.</small>}
+                            <label htmlFor="username">Username</label>
+                            <InputText id="username" value={this.state.vet.username} onChange={(e) => this.onInputChange(e, 'username')} required autoFocus className={classNames({ 'p-invalid': this.state.submitted && !this.state.vet.username })} />
+                            {this.state.submitted && !this.state.vet.username && <small className="p-error">Username é obrigatório.</small>}
+                        </div>
+                    </div>
+
+                    <div className="formgrid grid">
+                        <div className="field col">
+                            <label htmlFor="crmv">CRMV</label>
+                            <InputText id="crmv" value={this.state.vet.crmv} onChange={(e) => this.onInputChange(e, 'crmv')} required autoFocus className={classNames({ 'p-invalid': this.state.submitted && !this.state.vet.crmv })} />
+                            {this.state.submitted && !this.state.vet.crmv && <small className="p-error">CRMV é obrigatória.</small>}
+                        </div>
+                        <div className="field col">
+                            <label htmlFor="cpf_cnpj">CPF-CNPJ</label>
+                            <InputText id="cpf_cnpj" value={this.state.vet.cpf_cnpj} onChange={(e) => this.onInputChange(e, 'cpf_cnpj')} required autoFocus className={classNames({ 'p-invalid': this.state.submitted && !this.state.vet.cpf_cnpj })} />
+                            {this.state.submitted && !this.state.vet.cpf_cnpj && <small className="p-error">CPF-CNPJ é obrigatório.</small>}
+                        </div>
+                    </div>
+                    <div className="formgrid grid">
+                        <div className="field col">
+                            <label htmlFor="password">Senha</label>
+                            <InputText id="password" type="password" value={this.state.vet.password} onChange={(e) => this.onInputChange(e, 'password')} required autoFocus className={classNames({ 'p-invalid': this.state.submitted && !this.state.vet.password })} />
+                            {this.state.submitted && !this.state.vet.password && <small className="p-error">Senha é obrigatória.</small>}
+                        </div>
+                        <div className="field col">
+                            <label htmlFor="passwordconfirm">Senha</label>
+                            <InputText id="passwordconfirm" type="password" value={this.state.vet.passwordconfirm} onChange={(e) => this.onInputChange(e, 'passwordconfirm')} required autoFocus className={classNames({ 'p-invalid': this.state.submitted && !this.state.vet.passwordconfirm })} />
+                            {this.state.submitted && !this.state.vet.passwordconfirm && <small className="p-error">Senha é obrigatório.</small>}
                         </div>
                     </div>
 
@@ -356,7 +376,7 @@ export class Veterinario extends React.Component {
                 <Dialog visible={this.state.deleteVetDialog} style={{ width: '450px' }} header="Confirm" modal footer={deleteVetDialogFooter} onHide={this.hideDeleteVetDialog}>
                     <div className="confirmation-content">
                         <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem'}} />
-                        {this.state.pet && <span>Você tem certeza que deseja deletar esse pet? <b>{this.state.pet.nome}</b>?</span>}
+                        {this.state.vet && <span>Você tem certeza que deseja deletar esse Veterinario? <b>{this.state.vet.nome}</b>?</span>}
                     </div>
                 </Dialog>
 
