3fdeb8668b2e85397004f32fd76866c1801cd41e
Stengrat <ygorsa1998@hotmail.com>
2022-08-17 11:16:09 -0300

    [MOV-021] Adicionado validaçãõ de CPF e CNPJ

--- backend/src/medonvet/settings.py
+++ backend/src/medonvet/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'medonvet.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -127,11 +128,13 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
 
-CSRF_COOKIE_DOMAIN = 'localhost:3000'
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
+    'http://localhost:8000',
+    'https://localhost:8000',
 ]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
@@ -151,6 +154,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = 'media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
